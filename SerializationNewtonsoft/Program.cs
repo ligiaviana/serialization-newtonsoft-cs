4691ad47e6b5690bab7a9253ef7ea5b5640947b6
Ligia Viana <ligia.viana.azevedo@gmail.com>
2023-09-26 10:45:06 -0300

    serialization-newtonsoft-cs

---
+++ SerializationNewtonsoft/Program.cs
@@ -0,0 +1,28 @@
+﻿using SerializationNewtonsoft.Models;
+using System;
+using Newtonsoft.Json;
+
+namespace SerializationNewtonsoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataAtual = DateTime.Now;
+
+            List<Venda> listaVendas = new List<Venda>();
+
+            Venda v1 = new Venda(1, "Material de escritório", 25.00M, dataAtual);
+            Venda v2 = new Venda(2, "Licença de Software", 110.00M, dataAtual);
+
+            listaVendas.Add(v1);
+            listaVendas.Add(v2);
+
+            string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+            File.WriteAllText("Arquivos/vendas.json", serializado);
+
+            Console.WriteLine(serializado);
+        }
+    }
+}
